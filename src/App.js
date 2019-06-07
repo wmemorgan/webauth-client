58935cd991fa5d12b4ba07454116dfe487de4a70
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-08 01:24:53 +0800

    Setup app boilerplate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AppContainer from './components/DesignComponents/AppStyles'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppContainer>
+      <h2>User List</h2>
+    </AppContainer>
+  )
 }
 
 export default App;
