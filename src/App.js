4473efa5b84e91758288194b5db8ba33e2ff833a
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-08 02:56:25 +0800

    Add user registration form

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import AppContainer from './components/DesignComponents/AppStyles'
 
 import UserList from './components/UserComponents/UserList'
-
+import Register from './components/SharedComponents/Register'
 const App = () => {
   return (
     <AppContainer>
+      <Register add/>
       <UserList />
     </AppContainer>
   )
