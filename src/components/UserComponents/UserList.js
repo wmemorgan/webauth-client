9f5be6cdb6b8eab091301ef8d941677274143e92
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-11 22:39:23 +0800

    Update conditional rendering

--- src/components/UserComponents/UserList.js
+++ src/components/UserComponents/UserList.js
@@ -3,13 +3,11 @@ import React from 'react'
 import Loader from 'react-loader-spinner'
 import * as S from './UserStyles'
 
-
 const UserList = props => {
-  console.log(`UserList props: `, props)
   return (
     <S.UserListContainer>
       <h2>User List</h2>
-      {props.status === 200 ?
+      {props.errorMessage ? props.errorMessage :
         (props.userList.length > 0 ? (props.userList.map(user => (
           <S.Preview key={user.id}>
             <div>
@@ -23,8 +21,7 @@ const UserList = props => {
             <h2>Loading...</h2>
             <Loader type="Puff" color='#265077' height='60' width='60' />
           </S.SpinnerContainer>
-          )) :
-        <div>{props.errorMessage}</div>
+        ))
       }
     </S.UserListContainer>
   )
