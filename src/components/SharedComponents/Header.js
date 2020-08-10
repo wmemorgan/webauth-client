46bbd292b644f72fdba83db3c494da2af2bbc4bc
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-08-10 22:28:02 +0800

    Update navigation menu

--- src/components/SharedComponents/Header.js
+++ src/components/SharedComponents/Header.js
@@ -1,66 +1,61 @@
-import React, { Component } from 'react'
-import { NavLink, withRouter } from 'react-router-dom'
+import React, { Component } from "react";
+import { NavLink, withRouter } from "react-router-dom";
 
-import * as S from './HeaderStyles'
-import MobileMenuIcon from '../DesignComponents/MobileMenuIcon'
-import CloseIconMobile from '../DesignComponents/CloseIconMobile'
+import * as S from "./HeaderStyles";
+import MobileMenuIcon from "../DesignComponents/MobileMenuIcon";
+import CloseIconMobile from "../DesignComponents/CloseIconMobile";
 
 class Header extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { show: false }
-  }
+	constructor(props) {
+		super(props);
+		this.state = { show: false };
+	}
 
-  logout = () => {
-    localStorage.removeItem('jwt')
-    this.props.history.push('/signin')
-  }
+	logout = () => {
+		localStorage.clear();
+		this.props.history.push("/login");
+	};
 
-  // Open and close specific navigation elements
-  toggleDisplay = () => {
-    this.setState(prevState => (
-      {
-        show: !prevState.show
-      }
-    ))
-  }
+	// Open and close specific navigation elements
+	toggleDisplay = () => {
+		this.setState((prevState) => ({
+			show: !prevState.show,
+		}));
+	};
 
-  render() {
-    const token = localStorage.getItem('jwt')
+	render() {
+		const token = localStorage.getItem("token");
 
-    return (
-      <S.HeaderContainer>
-        <MobileMenuIcon
-          {...this.state}
-          onClick={this.toggleDisplay}
-        >
-          <i className="fas fa-bars"></i>
-        </MobileMenuIcon>
-        <CloseIconMobile
-          {...this.state}
-          onClick={this.toggleDisplay}
-        >
-          <i className="fa fa-times" aria-hidden="true"></i>
-        </CloseIconMobile>
-        <S.Nav {...this.state}>
-          <NavLink exact to='/' onClick={this.toggleDisplay}>
-            Home
-        </NavLink>
-        <S.CrudNav>
-          {!token ?
-            <NavLink to='/signin' onClick={this.toggleDisplay}>
-              Sign In
-            </NavLink> :
-            <div className="logout" onClick={this.logout}>Log Out</div>
-          }
-          <NavLink to='/signup' onClick={this.toggleDisplay}>
-            Sign Up
-          </NavLink>
-        </S.CrudNav>
-        </S.Nav>
-      </S.HeaderContainer>
-    )
-  }
+		return (
+			<S.HeaderContainer>
+				<MobileMenuIcon {...this.state} onClick={this.toggleDisplay}>
+					<i className="fas fa-bars"></i>
+				</MobileMenuIcon>
+				<CloseIconMobile {...this.state} onClick={this.toggleDisplay}>
+					<i className="fa fa-times" aria-hidden="true"></i>
+				</CloseIconMobile>
+				<S.Nav {...this.state}>
+					<NavLink exact to="/" onClick={this.toggleDisplay}>
+						Home
+					</NavLink>
+					<S.CrudNav>
+						{!token ? (
+							<NavLink to="/login" onClick={this.toggleDisplay}>
+								Sign In
+							</NavLink>
+						) : (
+							<div className="logout" onClick={this.logout}>
+								Log Out
+							</div>
+						)}
+						<NavLink to="/signup" onClick={this.toggleDisplay}>
+							Sign Up
+						</NavLink>
+					</S.CrudNav>
+				</S.Nav>
+			</S.HeaderContainer>
+		);
+	}
 }
 
-export default withRouter(Header)
+export default withRouter(Header);
