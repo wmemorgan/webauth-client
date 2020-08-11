f564898628c72b53867f8c3013cdd7e14bd401ef
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-08-11 14:37:06 +0800

    Add user sign up feature

--- src/components/AuthComponents/Register.js
+++ src/components/AuthComponents/Register.js
@@ -1,126 +1,170 @@
-import React, { Component } from 'react'
-import axios from 'axios' /** External API calls only **/
+import React, { Component } from "react";
+import axios from "axios"; /** External API calls only **/
 //import { connect } from 'react-redux' /** Redux only **/
-import { EventEmitter } from '../../utils/events'
+//import { EventEmitter } from "../../utils/events";
 
-import * as S from '../SharedComponents/FormStyles'
-import Button from '../DesignComponents/Button'
+import * as S from "../SharedComponents/FormStyles";
+import Button from "../DesignComponents/Button";
 
 //import {  } from '../../actions' /** Redux only **/
 
 class Form extends Component {
-  state = {
-    id: '',
-    username: '',
-    password: '',
-    department: '',
-    status: null,
-    errorMesage: ''
-  }
+	state = {
+		username: '',
+		firstname: '',
+		lastname: '',
+		primaryemail: '',
+		password: "",
+		status: null,
+		errorMesage: "",
+	};
 
-  handleInput = e => {
-    this.setState({ [e.target.id]: e.target.value });
-  }
+	handleInput = (e) => {
+		this.setState({ [e.target.id]: e.target.value });
+	};
 
-  addData = async e => {
-    // prevent default
-    e.preventDefault()
+	addData = async (e) => {
+		// prevent default
+		e.preventDefault();
 
-    // gather form data
-    let newRecord = {
-      username: this.state.username,
-      password: this.state.password,
-      department: this.state.department
-    }
+		// gather form data
+		const newRecord = {
+			username: this.state.username,
+			firstname: this.state.firstname,
+			lastname: this.state.lastname,
+			primaryemail: this.state.primaryemail,
+			password: this.state.password
+		};
 
-    // send new record to api
-    try {
-      let endpoint = '/auth/register'
-      let data = await axios.post(endpoint, newRecord)
-      localStorage.setItem('jwt', data.data.token)
-      this.setState({ status: data.status })
-    }
-    catch (err) {
-      console.error(err.response)
-      this.setState({ 
-        status: err.status,
-        errorMessage: err.response.data.message 
-      })
-    }
+		// send new record to api
+		try {
+			const endpoint = "/users/user";
+			const data = await axios.post(endpoint, newRecord);
+			console.log(`ADD USER: `, data);
+			if (data.status === 201) {
+				this.setState({ status: data.status });
+				this.props.history.push("/login");
+			}
+		} catch (err) {
+			console.error(err.response);
+			this.setState({
+				status: err.status,
+				errorMessage: err.response.data.error,
+			});
+		}
 
-    // reset form fields
-    this.setState({
-      username: '',
-      password: '',
-      department: ''
-    })
-    
-    EventEmitter.dispatch('getData')
-    this.props.history.push('/users') /** React-Router only **/
-  }
+		// reset form fields
+		this.setState({
+			username: "",
+			firstname: "",
+			lastname: "",
+			primaryemail: "",
+			password: "",
+		});
 
-  submitHandler = e => {
-    e.preventDefault()
-    if (this.props.update) {
-      this.updateRecord(e)
-    } else if (this.props.delete) {
-      this.deleteData(e)
-    } else {
-      this.addData(e)
-    }
-  }
+		// EventEmitter.dispatch("getData");
+		// this.props.history.push("/users"); /** React-Router only **/
+	};
 
-  componentDidMount() {
-    if(this.props.update) {
-      this.prePopulateForm(this.props.id)
-    }
-  }
+	submitHandler = (e) => {
+		e.preventDefault();
+		if (this.props.update) {
+			this.updateRecord(e);
+		} else if (this.props.delete) {
+			this.deleteData(e);
+		} else {
+			this.addData(e);
+		}
+	};
 
-  render() {
-    return (
-      <S.FormContainer {...this.props}>
-        <div className="windowFrame"></div>
-        <form onSubmit={this.submitHandler}>
-          {(this.props.update || this.props.delete) &&
-            <input name="id" type="number"
-              placeholder="ID" onChange={this.handleInput}
-              value={this.state.id}
-            />
-          }
-          {!this.props.delete && (
-            <main>
-              <label htmlFor="username"/>  
-              <input id="username" type="text"
-                placeholder="Username" onChange={this.handleInput}
-                value={this.state.username}
-                required
-              />
-              <label htmlFor="password" />
-              <input id="password" type="password"
-                placeholder="Password" onChange={this.handleInput}
-                value={this.state.password}
-                required
-              />
-              <label htmlFor="department" />
-              <input id="department" type="text"
-                placeholder="Department" onChange={this.handleInput}
-                value={this.state.department}
-                required
-              />
-            </main>
-          )}
-          <Button type="submit" {...this.props}>Sign Up</Button>
-        </form>
-      </S.FormContainer>
-    )
-  }
+	componentDidMount() {
+		if (this.props.update) {
+			this.prePopulateForm(this.props.id);
+		}
+	}
+
+	render() {
+		return (
+			<S.FormContainer {...this.props}>
+				<div className="windowFrame"></div>
+				<form onSubmit={this.submitHandler}>
+					{(this.props.update || this.props.delete) && (
+						<input
+							name="id"
+							type="number"
+							placeholder="ID"
+							onChange={this.handleInput}
+							value={this.state.id}
+						/>
+					)}
+					{!this.props.delete && (
+						<main>
+							<label htmlFor="username" />
+							<input
+								id="username"
+								type="text"
+								placeholder="Username"
+								onChange={this.handleInput}
+								value={this.state.username}
+								required
+							/>
+							<label htmlFor="firstname" />
+							<input
+								id="firstname"
+								type="text"
+								placeholder="First Name"
+								onChange={this.handleInput}
+								value={this.state.firstname}
+								required
+							/>
+							<label htmlFor="lastname" />
+							<input
+								id="lastname"
+								type="text"
+								placeholder="Last Name"
+								onChange={this.handleInput}
+								value={this.state.lastname}
+								required
+							/>
+							<label htmlFor="primaryemail" />
+							<input
+								id="primaryemail"
+								type="text"
+								placeholder="Email"
+								onChange={this.handleInput}
+								value={this.state.primaryemail}
+								required
+							/>
+							<label htmlFor="password" />
+							<input
+								id="password"
+								type="password"
+								placeholder="Password"
+								onChange={this.handleInput}
+								value={this.state.password}
+								required
+							/>
+						</main>
+					)}
+					<Button type="submit" {...this.props}>
+						Sign Up
+					</Button>
+				</form>
+				{this.state.errorMessage !== null ? (
+					<p>{this.state.errorMessage}</p>
+				) : (
+					""
+				)}
+			</S.FormContainer>
+		);
+	}
 }
 
-export default Form
+export default Form;
 
 /** Redux only **/
-// export default connect(null, { 
-//   addData: '', 
+// export default connect(null, {
+//   addData: '',
 //   updateData: '',
 //   deleteData: ''
 // })(Form)
