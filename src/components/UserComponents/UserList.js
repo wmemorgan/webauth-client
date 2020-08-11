6c766962c3cb9556f1a209993058c3c3c0386fd9
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-08-11 13:02:29 +0800

    Update conditional render

--- src/components/UserComponents/UserList.js
+++ src/components/UserComponents/UserList.js
@@ -7,29 +7,29 @@ import requiresAuth from '../AuthComponents/requiresAuth'
 import * as S from './UserStyles'
 
 const UserList = props => {
+  console.log(`USERLIST: `, props);
   return (
-    <S.UserListContainer>
-      <h2>User List</h2>
-      {props.errorMessage ? props.errorMessage :
-        (props.userList.length > 0 ? (props.userList.map(user => (
-          <Link key={user.id} to={`/users/${user.id}`}>
-            <S.Preview>
-              <div>
-                {user.username}
-              </div>
-              <div>
-                id: {user.id}
-              </div>
-            </S.Preview>
-          </Link>))) : (
-          <S.SpinnerContainer>
-            <h2>Loading...</h2>
-            <Loader type="Puff" color='#265077' height='60' width='60' />
-          </S.SpinnerContainer>
-        ))
-      }
-    </S.UserListContainer>
-  )
+		<S.UserListContainer>
+			<h2>User List</h2>
+			{props.status !== 200 ? (
+				<h3>{props.errorMessage}</h3>
+			) : props.userList.length > 0 ? (
+				props.userList.map((user) => (
+					<Link key={user.userid} to={`/users/${user.userid}`}>
+						<S.Preview>
+							<div>{user.username}</div>
+							<div>id: {user.userid}</div>
+						</S.Preview>
+					</Link>
+				))
+			) : (
+				<S.SpinnerContainer>
+					<h2>Loading...</h2>
+					<Loader type="Puff" color="#265077" height="60" width="60" />
+				</S.SpinnerContainer>
+			)}
+		</S.UserListContainer>
+	);
 }
 
 export default requiresAuth(UserList)
