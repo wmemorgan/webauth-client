a203eca893cf095f28c8a79d7f0a727a85cded60
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-08-11 11:35:02 +0800

    Update authentication token

--- src/components/AuthComponents/requiresAuth.js
+++ src/components/AuthComponents/requiresAuth.js
@@ -1,32 +1,37 @@
-import React from 'react'
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
 
 // Pass authentication token to API call
 axios.interceptors.request.use(
-  function(requestConfig) {
-    requestConfig.headers.authorization = localStorage.getItem('jwt')
-    return requestConfig
-  },
-  function(error) {
-    return Promise.reject(error)
-  }
-)
+	function (requestConfig) {
+		if (localStorage.getItem("token")) {
+			requestConfig.headers.authorization = `Bearer ${localStorage.getItem(
+				"token"
+			)}`;
+		}
+		return requestConfig;
+	},
+	function (error) {
+		return Promise.reject(error);
+	}
+);
 
 // Display component if user has been authenticated
-const Authenticated = Component => {
-  return class extends React.Component {
-    render () {
-      const token = localStorage.getItem('jwt')
-      return (
-        <>
-          {token ? 
-            <Component {...this.props} /> :
-            <h1>Please login to access page.</h1>
-          }
-        </>
-      )
-    }
-  }
-}
+const Authenticated = (Component) => {
+	return class extends React.Component {
+		render() {
+			const token = localStorage.getItem("token");
+			return (
+				<>
+					{token ? (
+						<Component {...this.props} />
+					) : (
+						<h1>Please login to access page.</h1>
+					)}
+				</>
+			);
+		}
+	};
+};
 
-export default Authenticated
+export default Authenticated;
